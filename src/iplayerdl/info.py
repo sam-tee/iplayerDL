588d336eab84a469531ae129bb690117c1606af6
Sam Tee <93236986+sam-tee@users.noreply.github.com>
2026-04-19 04:54:58 +0100

    fix: fixed media info fetching

--- src/iplayerdl/info.py
+++ src/iplayerdl/info.py
@@ -1,79 +1,76 @@
+import dotenv
 from tmdbv3api import TV, Movie, Season, TMDb
 from tmdbv3api.exceptions import TMDbException
 
 tmdb = TMDb()
 
 
-def title2show_data(title: str) -> dict:
+def title2show_data(title: str) -> dict[str, str]:
     """
     Converts BBC title of form <Show Name>, Series <>, <Episode Name>
     to show_data dict with keys show_name, series_num, episode_name
     """
     parts = title.split(", ", maxsplit=3)
     if len(parts) == 3:
+        series = parts[1].split(" ")
         return {
             "show_name": parts[0],
-            "series_num": parts[1][-1] if parts[1][-1].isdigit() else "0",
+            "series_num": series[-1] if series[-1].isnumeric() else "0",
             "episode_name": parts[2],
         }
     else:
-        return {"show_name": parts[0], "series_num": "0", "episode_name": parts[1]}
+        return {"show_name": parts[0], "series_num": "0", "episode_name": parts[-1]}
 
 
-def find_series(show_data: dict):
+def find_series(show_data: dict) -> str | None:
     """
-    Maps dict with keys: {show_name, series_num, episode_name} ->
-        dict with keys: {show_name, series_num, year, episode_num, episode_name}
+    Maps dict with keys: {show_name, series_num, episode_name} -> str of Jellyfin style mapping
     """
     tv = TV()
     season = Season()
     search_results = tv.search(show_data["show_name"])
-    for i, show in enumerate(search_results):
-        if i >= 3:
-            return
+    if search_results.total_results == 0:
+        # no matches found for tv show
+        return None
+    results = list(search_results.results)[:3]
+    for show in results:
         try:
-            details = season.details(show.id, show_data["series_num"])
-            episodes = details.episodes
+            episodes = season.details(show.id, show_data["series_num"]).episodes
         except TMDbException:
             episodes = []
-        except KeyError:
-            episodes = []
-        for ep in episodes:
+        try:
+            specials = season.details(show.id, 0).episodes
+        except TMDbException:
+            specials = []
+        for ep in list(episodes) + list(specials):
             if ep.name.lower() == show_data["episode_name"].lower():
                 name_year = f"{show.name} ({show.first_air_date.split('-')[0]})"
-                season_num = int(show_data["series_num"])
-                season = f"Season {season_num:02d}" if season_num != 0 else "Specials"
-                return f"tv/{name_year}/{season}/{name_year} - S{season_num:02d}E{ep.episode_number:02d} - {ep.name}"
-        if show_data.get("series_num") != 0:
-            try:
-                specials = season.details(show.id, 0)
-                eps = specials.episodes
-            except TMDbException:
-                eps = []
-            except KeyError:
-                eps = []
-            for special in eps:
-                if special.name.lower() == show_data["episode_name"].lower():
-                    name_year = f"{show.name} ({show.first_air_date.split('-')[0]})"
-                    season_num = int(show_data["series_num"])
-                    season = (
-                        f"Season {season_num:02d}" if season_num != 0 else "Specials"
-                    )
-                    return f"tv/{name_year}/{season}/{name_year} - S{season_num:02d}E{ep.episode_number:02d} - {ep.name}"
+                s_num = ep.season_number
+                season = f"Season {s_num:02d}" if s_num != 0 else "Specials"
+                return f"tv/{name_year}/{season}/{name_year} - S{s_num:02d}E{ep.episode_number:02d} - {ep.name}"
 
 
 def find_movie(title: str) -> str | None:
+    """
+    Takes in title of film and return Jellyfin style partial mapping with film and year
+    If multiple films have same title and first result is not 5x more popular then returns
+        without year
+    """
     movie = Movie()
     search_results = movie.search(title)
-    if len(search_results) == 0:
-        return None
 
-    title = search_results[0].title
-    if len(search_results) == 2 and title == search_results[1].title:
+    if search_results.total_results == 0:
+        return None
+    elif search_results.total_results == 1:
+        title = search_results[0].title
+    elif search_results[0] == search_results[1].title:
+        # if top two have same title then multiple of same film exist
+        # checks if second one is five times less popular and if so returns
+        # the first else returns just title and choice can be made later
         m1 = movie.details(search_results[0].id)
         m2 = movie.details(search_results[1].id)
         if m1.popularity < m2.popularity * 5:
-            return f"{title}"
+            return f"film/{title}/{title}"
     year = search_results[0].release_date.split("-")[0]
     movie_name = f"{title} ({year})"
     return f"film/{movie_name}/{movie_name}"
@@ -85,3 +82,10 @@ def get_media_name(title: str) -> str | None:
         return tv_name
     movie_name = find_movie(title)
     return movie_name
+
+
+if __name__ == "__main__":
+    dotenv.load_dotenv()
+    title = "Doctor Who, Series 13, The Vanquishers"
+    data = title2show_data(title)
+    print(get_media_name(title))
--- src/iplayerdl/subtitles.py
+++ src/iplayerdl/subtitles.py
@@ -7,5 +7,5 @@ def convert_file(input_file: Path, output_file: Path):
     subs = pysubs2.load(str(input_file))
     output_file.parent.mkdir(exist_ok=True, parents=True)
     subs.save(str(output_file))
-    print(f"Converted: {input_file.name} -> {output_file.name}")
+    print(f"\033[36m[sorter]\033[0m Converted: {input_file.name} -> {output_file.name}")
     input_file.unlink()
