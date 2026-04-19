142b7af968240cab19b5da1380bc7d416670cc1e
Sam Tee <93236986+sam-tee@users.noreply.github.com>
2026-04-19 04:54:34 +0100

    feat: removed download progress hook in favour of post process pipeline

--- src/iplayerdl/download.py
+++ src/iplayerdl/download.py
@@ -10,28 +10,28 @@ from iplayerdl.subtitles import convert_file
 
 def get_info(url) -> dict:
     """Downloads information from url"""
+    print(f"\033[34m[yt-dlp]\033[0m Downloading info for: {url}")
     with yt_dlp.YoutubeDL({"quiet": True}) as ydl:
         return ydl.extract_info(url, download=False)
 
 
-def get_hook(q: Queue, folders: Folders):
-    def download_hook(d):
-        if d["status"] == "finished":
-            file = Path(d["filename"])
-            print(f"\033[34m[yt-dlp]\033[0m Finished Download: {file.name}")
-            title = file.name.split(".")[0]
-            media_name = get_media_name(title)
-            if file.suffix == ".ttml":
-                convert_file(file, folders.media_dir / f"{media_name}.en.srt")
-                return
-            task = Task(
-                input_file=file,
-                transcode_file=folders.transcode_dir / f"{media_name}.mp4",
-                output_file=folders.media_dir / f"{media_name}.mp4",
-            )
-            q.put(task)
-
-    return download_hook
+def post_download(q: Queue, folders: Folders, dl_path: Path):
+    title = dl_path.stem
+    print(f"\033[34m[yt-dlp]\033[0m Finished Download: {title}")
+    media_name = get_media_name(title)
+    if media_name is None:
+        print(f"\033[31mError: No matching TMDb entry found for {title}\033[0m")
+        return
+    sub_paths = list(dl_path.parent.glob(f"{title}.*.*"))
+    for file in sub_paths:
+        convert_file(file, folders.media_dir / f"{media_name}.en.srt")
+    q.put(
+        Task(
+            input_file=dl_path,
+            transcode_file=folders.transcode_dir / f"{media_name}.mp4",
+            output_file=folders.media_dir / f"{media_name}.mp4",
+        )
+    )
 
 
 def download_url(
@@ -43,9 +43,10 @@ def download_url(
     if opts is None:
         opts = {}
     opts["paths"] = {"home": str(folders.download_dir.resolve())}
-    opts["progress_hooks"] = opts.get("progress_hooks", []) + [get_hook(q, folders)]
     info = get_info(url)
     entries = info.get("entries", [info])
     with yt_dlp.YoutubeDL(opts) as ydl:
         for entry in entries:
             ydl.download([entry["webpage_url"]])
+            dl_path = Path(ydl.prepare_filename(entry)).resolve()
+            post_download(q, folders, dl_path)
