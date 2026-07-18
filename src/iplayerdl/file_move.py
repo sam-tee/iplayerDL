80433c27da5df1b653ca1280879b584284c21004
Sam Tee <sam.tee4@proton.me>
2026-07-18 03:43:40 +0100

    feat: updated remote move to use scp

--- src/iplayerdl/file_move.py
+++ src/iplayerdl/file_move.py
@@ -2,13 +2,16 @@ import shlex
 import shutil
 import subprocess
 from dataclasses import dataclass
-from pathlib import Path, PurePosixPath
+from pathlib import Path
 
 
 @dataclass
 class RemotePath:
     host: str
-    path: str
+    path: Path
+
+    def get_scp_host(self) -> str:
+        return f"{self.host}:{self.path}"
 
 
 def parse_remote_path(path: Path) -> RemotePath | None:
@@ -16,24 +19,15 @@ def parse_remote_path(path: Path) -> RemotePath | None:
     Detect paths of the form <host>:<path>.
     """
     path_str = str(path)
-    host, sep, remote_path = path_str.partition(":")
-    if not sep or not host or "/" in host or not remote_path:
+    # windows check - local
+    if path.drive:
         return None
-    return RemotePath(host=host, path=remote_path)
-
-
-def remote_parent(path: str) -> str:
-    return str(PurePosixPath(path).parent)
-
-
-def move_remote(src: Path, dst: RemotePath):
-    cmd = (
-        f"mkdir -p -- {shlex.quote(remote_parent(dst.path))} "
-        f"&& cat > {shlex.quote(dst.path)}"
-    )
-    with open(src.resolve(), "rb") as f:
-        subprocess.run(["ssh", dst.host, cmd], stdin=f, check=True)
-    src.unlink()
+    host, sep, remote_path = path_str.partition(":")
+    if not sep or not host or not remote_path:
+        return None
+    if "/" in host or "\\" in host:
+        return None
+    return RemotePath(host, Path(remote_path).expanduser().resolve())
 
 
 def move_file(src: Path, dst: Path):
@@ -42,4 +36,16 @@ def move_file(src: Path, dst: Path):
         dst.parent.mkdir(exist_ok=True, parents=True)
         shutil.move(src.resolve(), dst.resolve())
     else:
-        move_remote(src, remote_output)
+        subprocess.run(
+            [
+                "ssh",
+                remote_output.host,
+                f"mkdir -p -- {shlex.quote(str(remote_output.path.parent))}",
+            ],
+            check=True,
+        )
+        subprocess.run(
+            ["scp", str(src.resolve()), remote_output.get_scp_host()],
+            check=True,
+        )
+        src.unlink()
